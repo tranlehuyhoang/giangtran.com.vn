9757e5d00ba5ce801c8b9c523276edd92243ca0b
Huyh Huyhu <2509roblox@gmail.com>
2024-11-07 22:18:36 +0700

    done interaction

--- resources/views/livewire/products/interaction-buff/orders.blade.php
+++ resources/views/livewire/products/interaction-buff/orders.blade.php
@@ -1,10 +1,12 @@
 <div>
     <div>
         <div>
+
             <head>
-                
+
             </head>
-            
+            </head>
+
             <body>
                 <!-- loader ends-->
                 <!-- tap on top starts-->
@@ -23,40 +25,74 @@
                             <!-- Container-fluid starts-->
                             <div class="container-fluid">
                                 <div class="row">
+
                                     <div class="col-sm-12">
                                         <div class="card">
                                             <div class="card-header">
-                                                <h4>Sample Card</h4>
-                                                <p class="f-m-light mt-1">Here you can enter a sub-title for your card.</p>
+                                                <h4>Quản lý đơn hàng</h4>
+                                                <span><code>Khi gói hosting hết hạn quá 3 ngày hệ thống sẽ tự động xóa hosting của bạn</code></span>
                                             </div>
                                             <div class="card-body">
-                                                <p><strong>Web Design Trends: </strong> Stay up-to-date with the latest trends in
-                                                    web design, such as the use of animations, micro-interactions, dark mode, and
-                                                    unique navigation techniques.Keep your website's navigation simple and
-                                                    intuitive, allowing users to find what they need easily without overwhelming
-                                                    them with options.</p>
-                                                <p><strong>Design Tools: </strong> Information on popular design software like Adobe
-                                                    Photoshop, Sketch, Figma, or Adobe XD, along with tips and tricks for efficient
-                                                    workflow and collaboration.Compress and optimize images to improve website
-                                                    loading speed and overall performance, providing a better user experience.</p>
-                                                <p><strong>Front-End Development: </strong> A basic understanding of HTML, CSS, and
-                                                    JavaScript can enhance your web design skills, enabling you to create
-                                                    interactive and dynamic elements.Ensure sufficient contrast between text and
-                                                    background colors to enhance readability and accessibility, especially for users
-                                                    with visual impairments.</p>
+
+                                                <div class="table-responsive theme-scrollbar signal-table">
+                                                    <table class="table table-hover">
+                                                        <thead>
+                                                            <tr>
+                                                                <th scope="col">Id</th>
+                                                                <th scope="col">Ngày đặt </th>
+                                                                <th scope="col">Link</th>
+                                                                <th scope="col">Tổng tiền</th>
+                                                                <th scope="col">Bắt đầu </th>
+                                                                <th scope="col">Số lượng</th>
+                                                                <th scope="col">Dịch vụ </th>
+                                                                <th scope="col">Trạnhg thái </th>
+                                                                <th scope="col">Còn lại</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                                <th scope="row">1</th>
+                                                                <td>06/11/2024 19:55 </td>
+                                                                <td>https://example.com/object_31 </td>
+                                                                <td>₫0 </td>
+                                                                <td>35,803 </td>
+                                                                <td>44 </td>
+                                                                <td>ID 74 </td>
+                                                                <td>Hoàn thành </td>
+                                                                <td>10 </td>
+
+                                                            </tr>
+
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                
                                             </div>
+                                            <div class="card-footer ">
+                                                <nav aria-label="Page navigation example ">
+                                                    <ul class="pagination pagination-primary pagin-border-primary">
+                                                      <li class="page-item"><a class="page-link" href="javascript:void(0)">Previous</a></li>
+                                                      <li class="page-item"><a class="page-link" href="javascript:void(0)">1</a></li>
+                                                      <li class="page-item"><a class="page-link" href="javascript:void(0)">2</a></li>
+                                                      <li class="page-item"><a class="page-link" href="javascript:void(0)">3</a></li>
+                                                      <li class="page-item"><a class="page-link" href="javascript:void(0)">Next</a></li>
+                                                    </ul>
+                                                  </nav>
+                                              </div>
                                         </div>
                                     </div>
+
                                 </div>
                             </div>
                             <!-- Container-fluid Ends-->
                         </div>
-                  
+
                         <!-- footer start-->
                         @livewire('inc.footer')
-                      </div>
+                    </div>
                 </div>
-             
+
         </div>
     </div>
+
 </div>
--- resources/views/livewire/products/interaction-buff/price-lists.blade.php
+++ resources/views/livewire/products/interaction-buff/price-lists.blade.php
@@ -26,24 +26,37 @@
                                     <div class="col-sm-12">
                                         <div class="card">
                                             <div class="card-header">
-                                                <h4>Sample Card</h4>
-                                                <p class="f-m-light mt-1">Here you can enter a sub-title for your card.</p>
+                                                <h4>Sub tiktok</h4>
                                             </div>
                                             <div class="card-body">
-                                                <p><strong>Web Design Trends: </strong> Stay up-to-date with the latest trends in
-                                                    web design, such as the use of animations, micro-interactions, dark mode, and
-                                                    unique navigation techniques.Keep your website's navigation simple and
-                                                    intuitive, allowing users to find what they need easily without overwhelming
-                                                    them with options.</p>
-                                                <p><strong>Design Tools: </strong> Information on popular design software like Adobe
-                                                    Photoshop, Sketch, Figma, or Adobe XD, along with tips and tricks for efficient
-                                                    workflow and collaboration.Compress and optimize images to improve website
-                                                    loading speed and overall performance, providing a better user experience.</p>
-                                                <p><strong>Front-End Development: </strong> A basic understanding of HTML, CSS, and
-                                                    JavaScript can enhance your web design skills, enabling you to create
-                                                    interactive and dynamic elements.Ensure sufficient contrast between text and
-                                                    background colors to enhance readability and accessibility, especially for users
-                                                    with visual impairments.</p>
+                                                <div class="table-responsive theme-scrollbar">
+                                                    <table class="table">
+                                                      <thead>
+                                                        <tr class="border-bottom-primary">
+                                                          <th scope="col">Id</th>
+                                                          <th scope="col">Tên dịch vụ</th>
+                                                          <th scope="col">Giá /1000</th>
+                                                        </tr>
+                                                      </thead>
+                                                      <tbody>
+                                                        <tr class="border-bottom-secondary">
+                                                            <th scope="row">1</th>
+                                                            <td>TikTok HQ Followers [Refill: 30D] [Max: 1M] [Start Time: 0 - 3 Hrs] [Speed: 50K/D] 💧⛔️♻️</td>
+                                                            <td> <span class="badge badge-light-success">₫52,475</span></td>
+                                                          </tr>
+                                                        <tr class="border-bottom-secondary">
+                                                            <th scope="row">1</th>
+                                                            <td>TikTok HQ Followers [Refill: 30D] [Max: 1M] [Start Time: 0 - 3 Hrs] [Speed: 50K/D] 💧⛔️♻️</td>
+                                                            <td> <span class="badge badge-light-success">₫52,475</span></td>
+                                                          </tr>
+                                                        <tr class="border-bottom-secondary">
+                                                            <th scope="row">1</th>
+                                                            <td>TikTok HQ Followers [Refill: 30D] [Max: 1M] [Start Time: 0 - 3 Hrs] [Speed: 50K/D] 💧⛔️♻️</td>
+                                                            <td> <span class="badge badge-light-success">₫52,475</span></td>
+                                                          </tr>
+                                                         </tbody>
+                                                    </table>
+                                                  </div>
                                             </div>
                                         </div>
                                     </div>
