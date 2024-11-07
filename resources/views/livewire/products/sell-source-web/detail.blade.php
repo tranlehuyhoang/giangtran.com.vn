af81a6156cf25aecc853c93d800c73a33ac8542e
Huyh Huyhu <2509roblox@gmail.com>
2024-11-07 20:34:47 +0700

    done source web detail

--- resources/views/livewire/products/sell-source-web/detail.blade.php
+++ resources/views/livewire/products/sell-source-web/detail.blade.php
@@ -5,6 +5,7 @@
             <head>
                 <title>Mã nguồn Chi tiết</title>
             </head>
+            <link rel="stylesheet" href="/mofi/assets/css/vendors/swiper/swiper-bundle.min.css">
 
             <body>
                 <!-- loader ends-->
@@ -20,9 +21,193 @@
                         <!-- Page Sidebar Start-->
                         @livewire('inc.sidebar')
                         <!-- Page Sidebar Ends-->
-                        <div class="page-body"> 
-                            
-                        
+                        <div class="page-body">
+                            <div class="container-fluid">
+                                <div class="row   g-10">
+                                    <div class="col-xl-8 col-12">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <h4>Code Checkscam ToiUytin V4
+                                                </h4>
+                                            </div>
+                                            <div class="card-body">
+                                                <div class="swiper mouseweel-swiper swiper-h">
+                                                    <div class="swiper-wrapper">
+                                                        <div class="swiper-slide"> <img
+                                                                src="https://imgur.com/u7ALYR1.png" alt=""></div>
+                                                        <div class="swiper-slide"><img
+                                                                src="https://imgur.com/JlJCMNi.png" alt=""></div>
+                                                        <div class="swiper-slide"><img
+                                                                src="https://imgur.com/hMOTG7A.png" alt=""></div>
+                                                        <div class="swiper-slide"><img
+                                                                src="https://imgur.com/o2hnqpU.png" alt=""></div>
+                                                        <div class="swiper-slide"><img
+                                                                src="https://imgur.com/JWHMHTW.png" alt=""></div>
+                                                        <div class="swiper-slide"><img
+                                                                src="https://imgur.com/warzZfi.png" alt=""></div>
+                                                        <div class="swiper-slide"><img
+                                                                src="https://imgur.com/6VSLneE.png" alt=""></div>
+
+                                                    </div>
+                                                    <div class="swiper-pagination"></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-xl-4 col-12">
+                                        <div class="card">
+                                            <div class="card-header">
+                                                <h4>Thông tin chi tiết
+                                                </h4>
+                                            </div>
+                                            <div class="card-body">
+
+                                                <p>Rock Paper Scissors Various Dots Half Sleeves Girl’s Regular Fit
+                                                    T-Shirt I 100% Cotton T Shirt with Half Sleeve Round Neck I Regular
+                                                    Wear Solid Kids Tees and Black Sleeve.</p>
+                                                <hr>
+                                                <div>
+                                                    <table class="product-page-width">
+                                                        <tbody>
+                                                            <tr>
+                                                                <td> <b>Brand &nbsp;&nbsp;&nbsp;:</b></td>
+                                                                <td>Pixelstrap</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> <b>Availability &nbsp;&nbsp;&nbsp;:
+                                                                        &nbsp;&nbsp;&nbsp;</b></td>
+                                                                <td class="txt-success">In stock</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> <b>Seller &nbsp;&nbsp;&nbsp;:
+                                                                        &nbsp;&nbsp;&nbsp;</b></td>
+                                                                <td>ABC</td>
+                                                            </tr>
+                                                            <tr>
+                                                                <td> <b>Fabric &nbsp;&nbsp;&nbsp;:
+                                                                        &nbsp;&nbsp;&nbsp;</b></td>
+                                                                <td>Cotton</td>
+                                                            </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                                <hr>
+                                                <div class="row">
+                                                    <div class="col-md-4">
+                                                        <h5 class="f-w-600 product-title">share it</h5>
+                                                    </div>
+                                                    <div class="col-md-8">
+                                                        <div class="product-icon">
+                                                            <ul class="product-social">
+                                                                <li class="d-inline-block"><a
+                                                                        href="https://www.facebook.com/"
+                                                                        target="_blank"><i
+                                                                            class="fa fa-facebook"></i></a></li>
+                                                                <li class="d-inline-block"><a
+                                                                        href="https://accounts.google.com/"
+                                                                        target="_blank"><i
+                                                                            class="fa fa-google-plus"></i></a></li>
+                                                                <li class="d-inline-block"><a
+                                                                        href="https://twitter.com/" target="_blank"><i
+                                                                            class="fa fa-twitter"></i></a></li>
+                                                                <li class="d-inline-block"><a
+                                                                        href="https://www.instagram.com/"
+                                                                        target="_blank"><i
+                                                                            class="fa fa-instagram"></i></a></li>
+                                                                <li class="d-inline-block"><a href="https://rss.app/"
+                                                                        target="_blank"><i class="fa fa-rss"></i></a>
+                                                                </li>
+                                                            </ul>
+                                                            <form class="d-inline-block f-right"></form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <hr>
+                                                <div class="row">
+                                                    <div class="col-md-4">
+                                                        <h5 class="f-w-600 product-title">Rate Now</h5>
+                                                    </div>
+                                                    <div class="col-md-8">
+                                                        <div class="d-flex">
+                                                            <div class="br-wrapper br-theme-fontawesome-stars"><select
+                                                                    id="u-rating-fontawesome" name="rating"
+                                                                    autocomplete="off" style="display: none;">
+                                                                    <option value="1">1</option>
+                                                                    <option value="2">2</option>
+                                                                    <option value="3">3</option>
+                                                                    <option value="4">4</option>
+                                                                    <option value="5">5</option>
+                                                                </select>
+                                                                <div class="br-widget"><a href="#" data-rating-value="1"
+                                                                        data-rating-text="1"
+                                                                        class="br-selected br-current"></a><a href="#"
+                                                                        data-rating-value="2"
+                                                                        data-rating-text="2"></a><a href="#"
+                                                                        data-rating-value="3"
+                                                                        data-rating-text="3"></a><a href="#"
+                                                                        data-rating-value="4"
+                                                                        data-rating-text="4"></a><a href="#"
+                                                                        data-rating-value="5" data-rating-text="5"></a>
+                                                                </div>
+                                                            </div><span class="ms-1">(250 review)</span>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <hr>
+                                                <div class="m-t-15 btn-showcase"><a class="btn btn-primary"
+                                                        href="cart.html" title=""> <i
+                                                            class="fa fa-shopping-basket me-1"></i>Add To Cart</a><a
+                                                        class="btn btn-success" href="checkout.html" title=""> <i
+                                                            class="fa fa-shopping-cart me-1"></i>Buy Now</a><a
+                                                        class="btn btn-secondary" href="list-wish.html"> <i
+                                                            class="fa fa-heart me-1"></i>Add To WishList</a></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row   g-10">
+                                    <div class="card">
+                                        <div class="row product-page-main">
+                                          <div class="col-sm-12">
+                                            <ul class="nav nav-tabs border-tab nav-primary mb-0" id="top-tab" role="tablist">
+                                              <li class="nav-item"><a class="nav-link active" id="top-home-tab" data-bs-toggle="tab" href="#top-home" role="tab" aria-controls="top-home" aria-selected="false">Febric</a>
+                                                <div class="material-border"></div>
+                                              </li>
+                                              <li class="nav-item"><a class="nav-link" id="profile-top-tab" data-bs-toggle="tab" href="#top-profile" role="tab" aria-controls="top-profile" aria-selected="false">Video</a>
+                                                <div class="material-border"></div>
+                                              </li>
+                                              <li class="nav-item"><a class="nav-link" id="contact-top-tab" data-bs-toggle="tab" href="#top-contact" role="tab" aria-controls="top-contact" aria-selected="true">Details</a>
+                                                <div class="material-border"></div>
+                                              </li>
+                                              <li class="nav-item"><a class="nav-link" id="brand-top-tab" data-bs-toggle="tab" href="#top-brand" role="tab" aria-controls="top-brand" aria-selected="true">Brand</a>
+                                                <div class="material-border"></div>
+                                              </li>
+                                            </ul>
+                                            <div class="tab-content" id="top-tabContent">
+                                              <div class="tab-pane fade active show" id="top-home" role="tabpanel" aria-labelledby="top-home-tab">
+                                                <p class="mb-0 m-t-20">Fabric is a substance made of fibres that have been knitted, woven, or otherwise joined to form a flexible, durable, and multipurpose substance used for clothing and a variety of other purposes. Natural fibres, synthetic fibres, or a combination of both can be used to make fabrics."</p>
+                                                <p class="mb-0 m-t-20">The cotton plant's seed bag is the source of cotton, a natural fibre. It is a popular material for daily wear including t-shirts, shirts, dresses, and undergarments since it is soft, breathable, and comfy.Make sure the video is well-produced and has excellent graphics. To grab the audience's attention, use captivating pictures and original angles.</p>
+                                              </div>
+                                              <div class="tab-pane fade" id="top-profile" role="tabpanel" aria-labelledby="profile-top-tab">
+                                                <p class="mb-0 m-t-20">A product video's main objective is to illustrate the attributes and functionality of the item. Demonstrate the product in use while emphasising how it satisfies a need or an issue.Include client feedback or testimonials in the video, where appropriate. Genuine customer endorsements may increase credibility and trust.</p>
+                                              </div>
+                                              <div class="tab-pane fade" id="top-contact" role="tabpanel" aria-labelledby="contact-top-tab">
+                                                <p class="mb-0 m-t-20">At the conclusion of the video, include a strong and obvious call to action that instructs viewers on what actions to take next, such as visiting a website, making a purchase, or signing up for a newsletter.</p>
+                                                <p class="mb-0 m-t-20">
+                                                   It might be difficult to create new material. While attempting to express oneself artistically, there are numerous factors to consider, including analytics and video quality. Since content creation tools are intended to make the process of creating new material easier, they may relieve some of the headache and lessen your workload.</p>
+                                              </div>
+                                              <div class="tab-pane fade" id="top-brand" role="tabpanel" aria-labelledby="brand-top-tab">
+                                                <p class="mb-0 m-t-20">Pick audio and background music that supports the message of the video. Ensure that the audio is crystal clear and improves the viewing experience.</p>
+                                                <p class="mb-0 m-t-20">For patients to comprehend and recognise these medications for optimal usage, established/mature brands need a certain level of market familiarity, illness education, and patient engagement with HCPs. During the pandemic, as these companies' income profiles continue to worsen, so do the attention and funding they receive and the already patchy brand recognition they have in the healthcare ecosystem.</p>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+
+                                </div>
+                            </div>
+
                         </div>
 
                         <!-- footer start-->
@@ -32,7 +217,18 @@
                         .animated-ribbon .box {
                             top: -3rem;
                         }
+
+                        .swiper-slide>img {
+                            width: 100%;
+                            object-fit: contain;
+                        }
+
+                        .swiper.swiper-h {
+                            height: 100%;
+                        }
                     </style>
+                    <script src="/mofi/assets/js/swiper/swiper-bundle.min.js"></script>
+                    <script src="/mofi/assets/js/custom-swiper/custom_swiper1.js"></script>
                 </div>
 
         </div>
