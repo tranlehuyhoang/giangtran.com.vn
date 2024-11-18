3a2fe7ddc529b29e9a1ffb57a72d55f2c8734180
Huyh Huyhu <2509roblox@gmail.com>
2024-11-18 21:46:12 +0700

    done

--- public/assets/static/invoices.js
+++ public/assets/static/invoices.js
@@ -1,5 +1,4 @@
-
-function ShowHoaDon(){
+function ShowHoaDon() {
     const modalHoadon = `<div class="modal fade" id="modalHoaDon" tabindex="-1" role="dialog" aria-labelledby="exampleModal" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
@@ -9,7 +8,21 @@ function ShowHoaDon(){
               
               <div class="col-lg-12 mt-3" id="dmm_box">
                   <div class="card">
-                    <div class="card-body faq-body" style="max-height: 200px; overflow-y: auto;">
+              <div class="card-body faq-body" style="max-height: 200px; overflow-y: auto;">
+                                            
+                      <a href="/hoa-don/564229">
+                          <div class="d-flex updates-faq-main">
+                            <div class="flex-grow-1 updates-bottom-time">
+                              <p class="txt-dark"> Hóa Đơn <strong>#564229</strong>, Số Tiền: <strong class="txt-danger">12,000<sup>đ</sup></strong></p>
+                              <p> Thời Gian Tạo: 18/11/2024 - 09:30:27 </p>
+                            </div>
+                            
+                            <b class="text-warning"> Chờ Thanh Toán </b>                            
+                            </div>
+                          </a>
+                          
+                          <br>
+
                                             
                     </div>
                   </div>
@@ -26,33 +39,30 @@ function ShowHoaDon(){
       </div>
     </div>`;
     document.body.insertAdjacentHTML('beforeend', modalHoadon);
-    
+
     var myModal = new bootstrap.Modal(document.getElementById('modalHoaDon'), {
         keyboard: false
-      });
+    });
     myModal.show();
 }
 
 document.getElementById("number_bill").innerHTML = "0";
 
-function changeWallet(name, id){
+function changeWallet(name, id) {
     var data = {
         name: name,
         invoiceId: id,
     };
-    
+
     axios.get('/axios/change-wallet', {
-        params: data
-    })
-    .then(function (response) {
-        if(response.data.status == 'success'){
-            loadto('');
-        }
-    })
-    .catch(function (error) {
-        ShowModal('Lỗi', error, 'error');
-    });
+            params: data
+        })
+        .then(function(response) {
+            if (response.data.status == 'success') {
+                loadto('');
+            }
+        })
+        .catch(function(error) {
+            ShowModal('Lỗi', error, 'error');
+        });
 }
-
-
-
