14f60f5ed1345a5cba96b523d6b6117044777adc
2509roblox <2509roblox@gmail.com>
2024-12-14 18:07:13 +0700

    doen deploy

---
+++ hgdigital.sh
@@ -0,0 +1,26 @@
+cd /var/www/hgdigital.site
+sudo rm -rf ./*
+sudo rm -rf .[^.]*  # Xóa các tệp ẩn
+sudo git clone -b HuyHoang https://github.com/tranlehuyhoang/giangtran.com.vn.git ./ && \
+sudo composer install && \
+sudo cp .env.production .env && \
+sudo chmod -R 777 . && \
+sudo php artisan storage:link && \
+sudo npm i && \
+sudo npm run build && \
+
+# Xóa và tạo lại database
+DB_NAME="hgdigital"
+DB_USER="hgdigital"
+DB_PASSWORD="hgdigital"  # Thay đổi mật khẩu nếu cần
+
+# Kết nối vào MySQL
+sudo mysql -u root -p -e "
+DROP DATABASE IF EXISTS $DB_NAME;
+CREATE DATABASE $DB_NAME;
+CREATE USER IF NOT EXISTS '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'localhost';
+FLUSH PRIVILEGES;"
+
+# Nhập dữ liệu từ file SQL
+sudo mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME < /var/www/hgdigital.site/hgdigital.sql
